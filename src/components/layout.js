2d4d77dbd75982d7514cae5ec70215247f9622a6
lukejohnbrown@gmail.com <lukejohnbrown@gmail.com>
2020-04-21 18:27:55 +0100

    Define spacing theme values and add ThemeProvider

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
-import React from "react";
-import { createGlobalStyle } from "styled-components";
+import React from "react"
+import { createGlobalStyle, ThemeProvider } from "styled-components"
 
-const GlobalStyle = createGlobalStyle `
+const GlobalStyle = createGlobalStyle`
   * {
     box-sizing: border-box;
     padding: 0;
@@ -11,16 +11,33 @@ const GlobalStyle = createGlobalStyle `
   body {
     font-family: "Helvetica", "Arial", sans-serif;
   }
-`;
+`
 
+const theme = {
+  space: [
+    "0.25rem",
+    "0.5rem",
+    "0.75rem",
+    "1rem",
+    "1.5rem",
+    "2rem",
+    "3rem",
+    "4rem",
+    "6rem",
+    "8rem",
+    "12rem",
+    "16rem",
+    "24rem",
+  ],
+}
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <GlobalStyle />
       <main>{children}</main>
-    </>
+    </ThemeProvider>
   )
-};
+}
 
-export default Layout;
+export default Layout
